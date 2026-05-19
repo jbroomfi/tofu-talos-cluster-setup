29f7719a0608d4a1d4ec9b3086376ad13415e547
Jason Broomfield <Jason.Broomfield@auraq.com>
2026-05-19 15:41:44 +0000

    Updated Gareths main.tf file and moved some environment specific settings into terraform.tfvars.json.  Added an tfvars.json.example file to show what attributes need to be populated as well.

--- main.tf
+++ main.tf
@@ -1,22 +1,98 @@
-locals {
-    cluster_name = "talos-cluster"
-
-    pm_node = "pve-k8s"
-    pm_endpoint = "https://pve-k8s.broomfieldtech.net:8006/"
-    pm_tls_insecure = true
-
-    talos_version = "1.13.2"
-    talos_schematic_id = "f39a41492e6317d259e5f0d38afe198116b111d41b3bf1fdf63a528cae24eb66"
-}
-
-
 provider "proxmox" {
-  endpoint = local.pm_endpoint
-  insecure = local.pm_tls_insecure
+  endpoint = var.proxmox_url
+  insecure = var.tls_insecure
 
   ssh {
-    agent = true
+    agent    = true
     username = var.ssh_username
     password = var.ssh_password
   }
+}
+
+locals {
+
+  node               = var.proxmox_node
+  clustername        = var.talos_cluster_name
+  talos_version      = var.talos_version
+  talos_arch         = var.talos_arch
+  talos_schematic_id = var.talos_schematic_id
+
+  disksizecontrolprimary  = 16
+  disksizeworkerprimary   = 16
+  disksizeworkersecondary = 32
+  cpucores                = 2
+
+}
+
+module "talos" {
+  source  = "bbtechsys/talos/proxmox"
+  version = "0.1.6"
+
+  talos_cluster_name = local.clustername
+  talos_version      = local.talos_version
+  #talos_arch         = local.talos_arch
+  #talos_schematic_id = local.talos_schematic_id
+
+  proxmox_worker_vm_disk_size  = local.disksizeworkerprimary
+  proxmox_control_vm_disk_size = local.disksizecontrolprimary
+  proxmox_worker_vm_cores      = local.cpucores
+  proxmox_control_vm_cores     = local.cpucores
+
+  control_nodes = {
+    "w-K8-control-0" = local.node
+    "w-K8-control-1" = local.node
+
+  }
+
+  worker_nodes = {
+    "w-K8-worker-0" = local.node
+    "w-K8-worker-1" = local.node
+    "w-K8-worker-2" = local.node
+
+  }
+
+  worker_extra_disks = {
+    "w-K8-worker-0" = [
+      {
+        datastore_id = "local-lvm"
+        size         = local.disksizeworkersecondary
+      }
+    ],
+    "w-K8-worker-1" = [
+      {
+        datastore_id = "local-lvm"
+        size         = local.disksizeworkersecondary
+      }
+    ]
+    "w-K8-worker-2" = [
+      {
+        datastore_id = "local-lvm"
+        size         = local.disksizeworkersecondary
+      }
+    ]
+  }
+
+  control_plane_mac_addresses = {
+    "w-K8-control-0" = "bc:24:11:d4:d6:37"
+    "w-K8-control-1" = "bc:24:11:5d:f3:9b"
+  }
+
+  worker_mac_addresses = {
+    "w-K8-worker-0" = "bc:24:11:cd:18:27"
+    "w-K8-worker-1" = "bc:24:11:01:08:e0"
+    "w-K8-worker-2" = "bc:24:11:cd:fe:19"
+  }
+
+}
+
+output "talos_config" {
+  description = "Talos configuration file"
+  value       = module.talos.talos_config
+  sensitive   = true
+}
+
+output "kubeconfig" {
+  description = "Kubeconfig file"
+  value       = module.talos.kubeconfig
+  sensitive   = true
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,98 @@
 variable "ssh_username" {
-    description = "SSH username for Proxmox API access"
-    type        = string
+  description = "SSH username for Proxmox API access"
+  type        = string
 }
 
 variable "ssh_password" {
-    description = "SSH password for Proxmox API access"
-    type        = string
-    sensitive   = true
+  description = "SSH password for Proxmox API access"
+  type        = string
+  sensitive   = true
+}
+
+variable "proxmox_url" {
+  description = "Proxmox URL"
+  type        = string
+  default     = "https://proxmox.internal:8006/"
+}
+
+variable "proxmox_node" {
+  description = "Proxmox Node to deploy VMs on"
+  type        = string
+  default     = "proxmox"
+}
+
+variable "proxmox_control_vm_cores" {
+  description = "Number of CPU cores for the control VMs"
+  type        = number
+  default     = 4
+}
+
+variable "proxmox_worker_vm_cores" {
+  description = "Number of CPU cores for the worker VMs"
+  type        = number
+  default     = 4
+}
+
+variable "proxmox_control_vm_disk_size" {
+  description = "Proxmox control VM disk size in GB"
+  type        = number
+  default     = 16
+}
+
+variable "proxmox_worker_vm_disk_size" {
+  description = "Proxmox worker VM disk size in GB"
+  type        = number
+  default     = 32
+}
+
+variable "talos_cluster_name" {
+  description = "Name of the Talos cluster"
+  type        = string
+}
+
+variable "talos_arch" {
+  description = "Architecture of Talos to use"
+  type        = string
+  default     = "amd64"
+}
+
+variable "talos_version" {
+  description = "Version of Talos to use"
+  type        = string
+}
+
+variable "proxmox_iso_datastore" {
+  description = "Datastore to put the qcow2 image"
+  type        = string
+  default     = "local"
+}
+
+variable "tls_insecure" {
+  description = "Confirm insecure API access"
+  type        = bool
+  default     = true
+}
+
+variable "talos_schematic_id" {
+  # Generate your own at https://factory.talos.dev/
+  # The this id has these extensions:
+  # qemu-guest-agent (required)
+  # If you make your own make sure you check this extension
+  # The ID is independent of the version and architecture of the image
+  description = "Schematic ID for the Talos cluster"
+  type        = string
+  default     = "ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515"
+}
+
+variable "worker_extra_disks" {
+  # This allows for extra disks to be added to the worker VMs
+  # TODO - Should we allow other things like host PCI devices as well E.g., GPUs?
+  description = "Map of talos worker node name to a list of extra disk blocks for the VMs"
+  type = map(list(object({
+    datastore_id = string
+    size         = number
+    file_format  = optional(string)
+    file_id      = optional(string)
+  })))
+  default = {}
 }
